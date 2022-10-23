afc9c6804ab209578d52be026e985fb2ee89fef1
Bryan Lin <bryan.0919.lin@gmail.com>
2022-10-23 23:10:55 +0800

    test: add unit test for useDebounce

---
+++ hooks/__test__/useDebounce.test.ts
@@ -0,0 +1,32 @@
+import { renderHook, act } from "@testing-library/react";
+import { useDebounce } from "../useDebounce";
+
+const setup = (text: string, delay?: number) =>
+  renderHook(() => useDebounce(text, delay));
+
+describe("useDebounce", () => {
+  it("should get debounced text after default delay", () => {
+    const text = "bryan";
+    const { result } = setup(text);
+    expect(result.current).toBeUndefined();
+
+    setTimeout(() => {
+      expect(result.current).toBeUndefined();
+    }, 500);
+
+    setTimeout(() => {
+      expect(result.current).toBe(text);
+    }, 1000);
+  });
+
+  it("should get debounced text after specified delay", () => {
+    const text = "bryan";
+    const delay = 500;
+    const { result } = setup(text, delay);
+    expect(result.current).toBeUndefined();
+
+    setTimeout(() => {
+      expect(result.current).toBeUndefined();
+    }, delay);
+  });
+});
--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export function useDebounce<T>(value: T, delay = 1000) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState<T>();
 
   useEffect(() => {
     const handler = setTimeout(() => {
